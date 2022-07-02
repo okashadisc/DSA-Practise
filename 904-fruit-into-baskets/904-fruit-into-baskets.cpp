9f65035563ada08a71cf9f486b4a4de57cbd225b
okashadisc <86793461+okashadisc@users.noreply.github.com>
2022-07-02 01:12:03 -0700

    Time: 148 ms (91.66%), Space: 66.2 MB (92.25%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        
+        //   aaabbb c     
+        
         int n=fruits.size();
-        int a=(-1),b=(-1);
-        int curr_len=0;
-        int max_len=0;
+        int currLen=0,maxLen=INT_MIN;
+        int a=(-1) ,b=(-1);
         int b_count=0;
         
         for(int i=0;i<n;i++){
             int c=fruits[i];
-            if(c==a or c==b){
-                curr_len+=1;
+            if(c != a and c != b){
+                a=b;
+                b=c;
+                currLen=b_count+1;
+                b_count=1;
             }
-            else{
-                curr_len=b_count+1;
-            }
-            
-            if(c==b){
+            else if(c==b){
+                currLen+=1;
                 b_count+=1;
             }
-            else{
+            else if(c==a){
+                currLen+=1;
+                a=b;
+                b=c;
                 b_count=1;
             }
             
-            if(c!=b){
-                a=b;
-                b=c;
-            }
-            
-            max_len=max(max_len,curr_len);
+            maxLen=max(maxLen,currLen);
         }
         
-        return max_len;
+        return maxLen;
         
     }
 };
