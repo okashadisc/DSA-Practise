25b67ba9df8bea5cfb6e863b947d7c848d2ccb9d
okashadisc <86793461+okashadisc@users.noreply.github.com>
2022-07-04 02:57:03 -0700

    Time: 6 ms (24.69%), Space: 5.9 MB (31.27%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        long long rev=0;
+        
+        while(x){
+            rev = (rev*10)+(x % 10);
+            x/=10;
+        }
+        
+        return (rev<INT_MIN or rev > INT_MAX)?0:rev;
+        
+    }
+};
