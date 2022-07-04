ebc850cae14760fdd3a28fec23d20a339b5122d4
okashadisc <86793461+okashadisc@users.noreply.github.com>
2022-07-04 03:25:17 -0700

    Time: 4 ms (44.83%), Space: 5.8 MB (76.07%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     int reverse(int x) {
         
-        long long rev=0;
+        int rev=0;
+        int lowerLimit=(-pow(2,31))/10;;
+        int upperLimit=(pow(2,31)-1)/10;
         
         while(x){
-            rev = (rev*10)+(x % 10);
-            x/=10;
+            int rem=x%10;
+           if(rev>=lowerLimit and rev<=upperLimit){
+               rev= rev*10 + rem;
+               x/=10;
+           }
+            else return 0;
+            
         }
         
-        return (rev<INT_MIN or rev > INT_MAX)?0:rev;
+        return rev;
         
     }
 };
