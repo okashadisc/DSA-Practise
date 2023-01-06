7e926b8667bc906f5e2205bb51fb6ebe2252f5e4
okashadisc <86793461+okashadisc@users.noreply.github.com>
2023-01-06 19:41:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (62.84%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    vector<int> dp;
     int dfs(int n,int k){
         //base case
-        if(n==0 and k>=2)return dp[n][k]=1;
+        if(n==0 ){
+            if(k>=2){
+                return dp[n]=1;
+            }
+            else return 0;
+        }
         
         //recursive case
-        if(dp[n][k] != (-1))return dp[n][k];
+        if(dp[n] != (-1))return dp[n];
         
         int ans= INT_MIN;
         for(int j=1;j<=n;j++){
@@ -20,12 +25,12 @@ public:
             }
         }
         
-        return dp[n][k]=ans;
+        return dp[n]=ans;
     }
     int integerBreak(int n) {
         
         int k=0;
-        dp.resize(60,vector<int>(1005,-1));
+        dp.resize(60,-1);
         return dfs(n,k);
         
     }
