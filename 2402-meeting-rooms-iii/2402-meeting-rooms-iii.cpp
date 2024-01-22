46eaa86f958d9db6193133f638ec9771fbc59901
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-01-22 22:04:30 +0530

    Time: 371 ms (95.26%), Space: 114 MB (17.26%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -0,0 +1,60 @@
+// typedef pair<long long,int> node;
+bool comp(vector<int> &a,vector<int> &b){
+        
+        return  a[0]<b[0];
+    }
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        
+        
+        // Early startTime and Early endTime
+        sort(meetings.begin(),meetings.end(),comp);
+        
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> q;
+        priority_queue<int,vector<int>,greater<int>> unused;
+        unordered_map<int,int> freq;
+        
+        for(int i=0;i<n;i++)unused.push(i);
+        
+        for(auto x: meetings){
+            
+            int start= x[0];
+            int end= x[1];
+            
+            // Enqueue all the meetings that can be attended
+            
+            while(!q.empty() and q.top().first<=start){
+                
+                int room= q.top().second;
+                q.pop();
+                unused.push(room);
+            }
+            
+            if(!unused.empty()){
+                
+                int earliestRoom= unused.top();
+                unused.pop();
+                freq[abs(earliestRoom)]+= 1;
+                q.push({end,earliestRoom});
+            }
+            else{
+                
+                pair<long long,int> earliestMeeting= q.top();
+                q.pop();
+                int earliestRoomAvailable= earliestMeeting.second;
+                long long endTime= earliestMeeting.first;
+                endTime+= (end-start);
+                freq[abs(earliestRoomAvailable)]+= 1;
+                q.push({endTime,earliestRoomAvailable});
+            }
+        }
+        
+        int maxi = 0;
+        for(int i = 1; i < n; i++) {
+            if(freq[i] > freq[maxi])
+                maxi = i;
+        }
+        return maxi;
+    }
+};
