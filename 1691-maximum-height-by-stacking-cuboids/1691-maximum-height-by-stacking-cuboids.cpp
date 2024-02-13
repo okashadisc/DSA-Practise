f972a2cbd6039b1e144886943005ee8b09e575d3
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-02-13 17:15:51 +0530

    Time: 0 ms (100.00%), Space: 12 MB (54.98%) - LeetHub

---
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxHeight(vector<vector<int>>& cuboids) {
+        int n= cuboids.size();
+        
+        vector<int> dp(n,0);
+        
+        // Sort Indivisual boxes to set height as (max) for each box
+        for(int i=0;i<n;i++){
+            
+            sort(cuboids[i].begin(),cuboids[i].end());
+        }
+        
+        
+        sort(cuboids.begin(),cuboids.end());
+        int mx= 0;
+        
+        for(int i=0;i<n;i++){
+            
+            dp[i]= cuboids[i][2];
+            for(int j=0;j<i;j++){
+                
+                if(cuboids[j][0]<=cuboids[i][0] and cuboids[j][1]<=cuboids[i][1] and cuboids[j][2]<=cuboids[i][2]){
+                    
+                    auto possibleAns= dp[j] + cuboids[i][2];
+                    dp[i]=max(dp[i],possibleAns);
+                }
+            }
+                mx=max(mx,dp[i]);
+        }
+        
+        return mx;
+    }
+};
