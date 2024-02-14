fa37f0bf7e603c9ed0e2ca60cc139d34ee77b3eb
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-02-14 15:48:35 +0530

    Time: 77 ms (45.71%), Space: 83.8 MB (22.07%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -11,22 +11,21 @@ public:
         //recursive case
         if(dp[x1][y1][y2] != (-1))return dp[x1][y1][y2];
         
+        int ans=0;
         if(x1==n-1 and y1==n-1)return grid[x1][y1];
         
-       int ans= grid[x1][y1];
-        if(x1 != x2) ans+= (grid[x2][y2]);
+        if(x1==x2 and y1==y2)ans+= grid[x1][y1];
+        else ans+= (grid[x1][y1] + grid[x2][y2]);
         
                                                  // (path1,path2)
         
-//         int option1= dfs(x1+1,y1,y2,grid,n);     // (down,down)
-//         int option2= dfs(x1,y1+1,y2,grid,n);     // (right,down)
-//         int option3= dfs(x1+1,y1,y2+1,grid,n);   // (down,right)
-//         int option4= dfs(x1,y1+1,y2+1,grid,n);   // (right,right)
+        int option1= dfs(x1+1,y1,y2,grid,n);     // (down,down)
+        int option2= dfs(x1,y1+1,y2,grid,n);     // (right,down)
+        int option3= dfs(x1+1,y1,y2+1,grid,n);   // (down,right)
+        int option4= dfs(x1,y1+1,y2+1,grid,n);   // (right,right)
         
-        int temp= max({dfs(x1,y1+1,y2+1,grid,n),dfs(x1+1,y1,y2+1,grid,n),dfs(x1,y1+1,y2,grid,n),dfs(x1+1,y1,y2,grid,n)});
-        ans+= temp;
-        
-        return dp[x1][y1][y2]= ans;
+        return dp[x1][y1][y2]= max({option1,option2,option3,option4}) + ans;
+  
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int n= grid.size();
