f3218c7e978a12cdc30780b4d4cb5fec94ec92ce
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-02-14 15:44:29 +0530

    Time: 81 ms (45.00%), Space: 84 MB (22.07%) - LeetHub

---
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<vector<int>>> dp;
+    int dfs(int x1,int y1,int y2,vector<vector<int>> &grid,int n){
+        
+        //base case
+        int x2= x1 + y1 - y2;
+        if(x1>=n or x2>=n or y1>=n or y2>=n or grid[x1][y1]==(-1) or grid[x2][y2]==(-1))return INT_MIN;
+
+        
+        //recursive case
+        if(dp[x1][y1][y2] != (-1))return dp[x1][y1][y2];
+        
+        if(x1==n-1 and y1==n-1)return grid[x1][y1];
+        
+       int ans= grid[x1][y1];
+        if(x1 != x2) ans+= (grid[x2][y2]);
+        
+                                                 // (path1,path2)
+        
+//         int option1= dfs(x1+1,y1,y2,grid,n);     // (down,down)
+//         int option2= dfs(x1,y1+1,y2,grid,n);     // (right,down)
+//         int option3= dfs(x1+1,y1,y2+1,grid,n);   // (down,right)
+//         int option4= dfs(x1,y1+1,y2+1,grid,n);   // (right,right)
+        
+        int temp= max({dfs(x1,y1+1,y2+1,grid,n),dfs(x1+1,y1,y2+1,grid,n),dfs(x1,y1+1,y2,grid,n),dfs(x1+1,y1,y2,grid,n)});
+        ans+= temp;
+        
+        return dp[x1][y1][y2]= ans;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n= grid.size();
+        
+        dp.resize(55,vector<vector<int>>(55,vector<int>(55,-1)));
+        return max(0,dfs(0,0,0,grid,n));
+    }
+};
