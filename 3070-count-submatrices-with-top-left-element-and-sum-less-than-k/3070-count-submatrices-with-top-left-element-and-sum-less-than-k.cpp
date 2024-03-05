9dc529092ebe7d05de7f77faea2c1d33756005b4
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-03-05 23:08:23 +0530

    Time: 201 ms (7.31%), Space: 99.4 MB (20.06%) - LeetHub

---
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int n= grid.size();
+        int m= grid[0].size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=m;j++){
+                
+                dp[i][j]= (dp[i][j-1] + dp[i-1][j] + grid[i-1][j-1]) - dp[i-1][j-1]; 
+            }
+        }
+        
+        long long count=0;
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=m;j++){
+                
+                if(dp[i][j]<=k)count+= 1;
+            }
+        }
+        
+        
+        return count;
+    }
+};
