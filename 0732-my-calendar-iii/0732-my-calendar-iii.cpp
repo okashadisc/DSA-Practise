c3e7ab267f3b2844d4068a288e2b3d04f1592be3
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-05-12 14:56:05 +0530

    Time: 297 ms (9.56%), Space: 280.6 MB (5.22%) - LeetHub

---
+++ 0732-my-calendar-iii/0732-my-calendar-iii.cpp
@@ -0,0 +1,181 @@
+class Node {
+    
+    public:
+    Node* left;
+    Node* right;
+    int val;
+    int lazy;
+    
+    Node(){
+        
+        left= NULL;
+        right= NULL;
+        val= 0;
+        lazy= 0;
+    }
+    
+//     void extend(){
+        
+//         left= new Node();
+//         right= new Node();
+//     }
+};
+class SegmentTree {
+    
+    public:
+    int n;
+    Node* root;
+    
+    SegmentTree(int _n){
+        
+       root= new Node();
+        
+        n= _n;
+    }
+    
+    void up(int left,int right,int val){
+        
+        updateRangeQuery(root,1,n,left,right,1);
+    }
+    
+    int query(int left,int right){
+        
+        return rangeSumQuery(root,1,n,left,right);
+    }
+    
+    void updateRangeQuery(Node* root,int start,int end,int left,int right,int val){
+        
+        if(root->lazy){
+            
+            root->val+= root->lazy;
+            if(start != end){
+                if(!root->left or !root->right){
+                    
+                    root->left= new Node();
+                    root->right= new Node();
+                }
+                
+                root->left->lazy+= root->lazy;
+                root->right->lazy+= root->lazy;
+                
+            }
+            
+            root->lazy=0;
+        }
+        
+        // No Overlap
+        
+        if(end<left or start>right or start>end)return;
+        
+        // Complete Overlap
+        
+        if(start>=left and end<=right){
+            
+            root->val+= (val);
+            
+            if(start != end){
+                
+                if(!root->left or !root->right){
+                    
+                    root->left= new Node();
+                    root->right= new Node();
+                }
+            
+                root->left->lazy+= (val);
+                root->right->lazy+= (val);
+            
+            }
+            
+            return;
+        }
+        
+        // Partial Overlap
+        
+        int mid= start + (end-start)/2;
+        
+        if(!root->left or !root->left){
+            
+            root->left= new Node();
+            root->right= new Node();
+        }
+        
+        updateRangeQuery(root->left,start,mid,left,right,val);
+        
+        updateRangeQuery(root->right,mid+1,end,left,right,val);
+        
+        root->val= max(root->left->val,root->right->val);
+        
+        
+    }
+    
+    int rangeSumQuery(Node* root,int start,int end,int left,int right){
+        
+        if(root->lazy){
+            
+            root->val+= (root->lazy);
+            
+            if(start != end){
+                
+                if(!root->left or !root->right){
+                    
+                    root->left= new Node();
+                    root->right= new Node();
+                }
+                
+                root->left->lazy+= (root->lazy);
+                root->right->lazy+= (root->lazy);
+            }
+            
+            root->lazy= 0;
+        }
+        
+        // No Overlap
+        
+        if(end<left or start>right or start>end)return 0;
+        
+        // Complete Overlap
+        
+        if(start>=left and end<=right){
+            
+            return root-> val;
+        }
+        
+        // Partial Overlap
+        
+        int mid= start + (end-start)/2;
+        
+        if(!root->left or !root->right){
+            
+            root->left= new Node();
+            root->right= new Node();
+        }
+        
+        return max(rangeSumQuery(root->left,start,mid,left,right),rangeSumQuery(root->right,mid+1,end,left,right));
+        
+        
+    }
+};
+class MyCalendarThree {
+public:
+    const int n= 1e9;
+    SegmentTree* st;
+    
+    
+    MyCalendarThree() {
+        
+        st= new SegmentTree(n);
+    }
+    
+    int book(int startTime, int endTime) {
+        
+        st->up(startTime,endTime-1,1);
+        
+        return st-> query(1,n);
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(startTime,endTime);
+ */
