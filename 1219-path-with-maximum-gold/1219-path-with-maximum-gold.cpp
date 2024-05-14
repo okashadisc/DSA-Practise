1e538096ecf41118ea4022c37231e411fb31f512
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-05-14 13:11:30 +0530

    Time: 336 ms (22.23%), Space: 13.7 MB (8.82%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -3,24 +3,26 @@ public:
     int ans;
     // vector<vector<int>> dp;
     
-    int dfs(int i,int j,vector<vector<int>> &grid,vector<vector<bool>> &visited,int n,int m){
+    void dfs(int i,int j,vector<vector<int>> &grid,vector<vector<bool>> &visited,int n,int m,int gold){
         
         //base case
-        if(i<0 or i>=n or j<0 or j>=m or visited[i][j] or !grid[i][j])return 0;
+        if(i<0 or i>=n or j<0 or j>=m or visited[i][j] or !grid[i][j])return;
         
         //recursive case
         // if(dp[i][j] != (-1))return dp[i][j];
         
         visited[i][j]= true;
         
-        int left= dfs(i,j-1,grid,visited,n,m);
-        int right= dfs(i,j+1,grid,visited,n,m);
-        int top= dfs(i-1,j,grid,visited,n,m);
-        int bottom= dfs(i+1,j,grid,visited,n,m);
+        gold+= grid[i][j];
+        ans= max(ans,gold);
+        
+        
+        dfs(i,j-1,grid,visited,n,m,gold);
+        dfs(i,j+1,grid,visited,n,m,gold);
+        dfs(i-1,j,grid,visited,n,m,gold);
+        dfs(i+1,j,grid,visited,n,m,gold);
         
         visited[i][j]= false;
-        
-        return max({left,right,top,bottom}) + grid[i][j];
         
     }
     int getMaximumGold(vector<vector<int>>& grid) {
@@ -35,7 +37,7 @@ public:
             for(int j=0;j<m;j++){
                 
                 vector<vector<bool>> visited(n,vector<bool>(m,false));
-                if(grid[i][j])ans= max(ans, dfs(i,j,grid,visited,n,m));
+                if(grid[i][j])dfs(i,j,grid,visited,n,m,0);
             }
         }
         
