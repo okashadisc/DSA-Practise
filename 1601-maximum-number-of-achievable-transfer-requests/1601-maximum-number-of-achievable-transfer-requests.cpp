522858e8cbca34b6d0f87931c27b235a82ff0b4a
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-09-06 21:16:26 +0530

    Time: 34 ms (59.92%), Space: 10.8 MB (79.17%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    int res;
-    vector<int> count;
-        void dfs(int indx,int n,vector<vector<int>> &requests,int numberOfCurrentRequests){
+    long long res;
+        int dfs(int indx,int n,vector<vector<int>> &requests,vector<int> &count){
         
         //base case
-        if(indx>=requests.size()){
+        if(indx==requests.size()){
             
             for(int i=0;i<n;i++){
                 
-                if(count[i] != 0)return;
+                if(count[i] != 0)return INT_MIN;
             }
-            
-            res=max(res,numberOfCurrentRequests);
-            return;
+            return 0;
         }
         
         //recursive case
         
         // Apply Principle of Inclusion-Exclusion
         
-        // Once I will Include
+        // Once I will Exclude
         
+            
+        int option2= 0 + dfs(indx+1,n,requests,count);
+        
+        // Once I will Include
         count[requests[indx][0]]-=1;;
         count[requests[indx][1]]+=1;
         
-        dfs(indx+1,n,requests,numberOfCurrentRequests+1);
+        int option1= 1 +  dfs(indx+1,n,requests,count);
+            
         
         count[requests[indx][0]]+=1;
         count[requests[indx][1]]-=1;
         
-        // Once I will exclude
-        
-        dfs(indx+1,n,requests,numberOfCurrentRequests);
-        
+        return max(option1,option2);
     }
     int maximumRequests(int n, vector<vector<int>>& requests) {
         
+        vector<int> count;
         count.resize(n,0);
         
         res=0;
-        dfs(0,n,requests,0);
+        return dfs(0,n,requests,count);
         
-        return res;
     }
 };
