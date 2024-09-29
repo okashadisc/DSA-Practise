1be2e3dc428dae2860a30338fc86764d4da54082
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-09-30 00:22:38 +0530

    Time: 114 ms (92.00%), Space: 92 MB (22.31%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -3,16 +3,50 @@ public:
     int maximumGap(vector<int>& nums) {
         int n= nums.size();
         
-        if(n<2)return 0;
-        sort(nums.begin(),nums.end());
+        if(n==1)return 0;
         
-        int maxDiff= INT_MIN;
+        vector<int> minimumOfBucket(n,INT_MAX);
+        vector<int> maximumOfBucket(n,INT_MIN);
+        
+        //  [Bucket 1]   [Bucket 2]   [Bucket 3]  [Bucket 4]
+        //     [0 7]        [8 15]       [16 23]    [24 31]
+        //                    [Range of Numbers]
+        
+        
+       int maximum= *max_element(nums.begin(),nums.end());
+        int minimum= *min_element(nums.begin(),nums.end());
+        
+        int bucketSize= (int) ceil((double)(maximum-minimum)/(n-1));
+        
+        if(bucketSize == 0)return 0;
+        
+        for(int i=0;i<n;i++){
+            
+            int number= nums[i];
+            int bucketIndx= (number-minimum)/bucketSize;
+            
+            minimumOfBucket[bucketIndx]=min(minimumOfBucket[bucketIndx],number);
+            maximumOfBucket[bucketIndx]= max(maximumOfBucket[bucketIndx],number);
+        }
+        
+        
+        //   [min  max]   [min   max]   [min   max]
+        
+        int previousMax= maximumOfBucket[0];
+        int maxDiff= bucketSize;
+        
+        if(minimum == maximum)return 0;
+        
         for(int i=1;i<n;i++){
             
-            maxDiff=max(maxDiff,nums[i]-nums[i-1]);
+            if(minimumOfBucket[i] == INT_MAX)continue;
+            
+            maxDiff=max(maxDiff, minimumOfBucket[i]-previousMax);
+            previousMax= maximumOfBucket[i];
         }
         
         
         return maxDiff;
+        
     }
 };
