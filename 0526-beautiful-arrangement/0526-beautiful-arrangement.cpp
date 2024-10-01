557aa1109c9fcdaec465d9239e9efa1874ad78b7
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-10-01 16:14:01 +0530

    Time: 4 ms (93.97%), Space: 56 MB (11.58%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    
     int dfs(int indx,vector<int> &nums,int n,int mask){
         
         //base case
-        if(indx>n)return 1;
+        if(indx>n)return dp[indx][mask]= 1;
         
         //recursive case
+        if(dp[indx][mask] != (-1))return dp[indx][mask];
         
         long long count=0;
         for(int i=0;i<nums.size();i++){
@@ -22,13 +25,14 @@ public:
         }
         
         
-        return count;
+        return dp[indx][mask]= count;
     }
     int countArrangement(int n) {
         
         vector<int> nums;
         for(int i=1;i<=n;i++)nums.push_back(i);
         
+        dp.resize(20,vector<int>(pow(2,15),-1));
         return dfs(1,nums,n,0);
     }
 };
