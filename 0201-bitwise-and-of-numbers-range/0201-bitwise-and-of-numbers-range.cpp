b2ddb4a886f54478d96d528ea8396870445866bb
okashadisc <86793461+okashadisc@users.noreply.github.com>
2024-10-09 22:53:49 +0530

    Time: 8 ms (50.97%), Space: 10.3 MB (61.06%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
         
+        // Keep Dropping the (lowest) (set) bits
         while(left<right){
             
             right= (right) & (right-1);
